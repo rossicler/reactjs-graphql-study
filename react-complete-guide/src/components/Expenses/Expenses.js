0d256529ccd271917571bf2420c60720eae9e88e
rossicler <rossiclerjr@gmail.com>
2021-08-17 22:30:23 -0300

    Added basic expenses project

---
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -0,0 +1,27 @@
+import ExpenseItem from "./ExpenseItem";
+import Card from "./../UI/Card";
+import "./Expenses.css";
+
+const Expenses = ({ expenses }) => {
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+    </Card>
+  );
+};
+
+export default Expenses;
